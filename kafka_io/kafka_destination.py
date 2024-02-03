2c71a6486e574a7d06160afad3d5203b86a98207
AgasthyaNG <agasthyanaib3@gmail.com>
2024-02-03 11:04:38 +1100

    🔥  remove undefined files

--- kafka_io/kafka_destination.py
+++
@@ -1,4 +0,0 @@
-# The destination for kafka messaging system is defined in this file
-# This will act as a produce for kafka topic and broker that will be hosted 
-# You can also test by running the kafka_destination.py locally 
-
