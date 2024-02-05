b1b0d0cd72880c9820f3df1225ced42a40f3737e
AgasthyaNG <agasthyanaib3@gmail.com>
2024-02-05 17:36:11 +1100

    🚨 Fixing lint warnings

--- transformation/custom/decodebase64.py
+++ transformation/custom/decodebase64.py
@@ -1,3 +1,14 @@
+"""This program reads the msg.value sent up by the transformation function and decode it using base64 library
+"""
 import base64
+
 def decodebase64(msg) -> str:
+    """_summary_
+
+    Args:
+        msg (_type_): base 64 encoded string to be decoded
+
+    Returns:
+        str: decode data 
+    """
     return(base64.b64decode(msg).decode("utf-8"))
