e7366bcbf09d2b54893ec8bf6fef869a27f48f34
AgasthyaNG <agasthyanaib3@gmail.com>
2024-02-05 17:32:38 +1100

    🏷️  Added docstring and some types

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
 import logging
-from kafka_io import kafka_source
 from abc import ABC, abstractmethod
+from kafka_io import kafka_source
 from transformation.transformation import transform
 
 logging.basicConfig(level=logging.INFO)
 
-class pons(ABC):
+TRANSFORMATIONS = False
+
+class Pons(ABC):
     """
         A description of the entire function, its parameters, and its return types.
     """
@@ -16,14 +23,14 @@ class pons(ABC):
         A description of the entire function, its parameters, and its return types.
         """
         pass
-    
+
     @abstractmethod
     def transformation(self):
         """
         A description of the entire function, its parameters, and its return types.
         """
         pass
-    
+
     @abstractmethod
     def destination(self):
         """
@@ -44,8 +51,11 @@ def run_application() -> None:
     logging.info("Starting the application")
     value = kafka_source.kafka_read_data({"bootstrap_servers": "localhost:9092"}, "kafka-topic")
     for msg in value:
-        logging.info(f'Received message: {msg.value.decode("utf-8")}')
-        print(transform(msg.value,"decodebase64","./transformation/custom/decodebase64.py"))
-    
+        logging.info('Received message: %s', msg.value.decode('utf-8'))
+        if TRANSFORMATIONS:
+            print(transform(msg.value,"decodebase64","./transformation/custom/decodebase64.py"))
+        else:
+            print(msg)
+
 if __name__ == '__main__':
     run_application()
