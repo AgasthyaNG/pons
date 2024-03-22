39645cbb7bee404074a25e1a0c96f18914f20da9
AgasthyaNG <agasthyanaib3@gmail.com>
2024-03-23 09:58:52 +1100

    ✨ destination pubsub

--- main.py
+++ main.py
@@ -3,30 +3,46 @@
     Returns:
         _type_: _description_
 """
+
 import logging
 from enum import Enum as enum
 from kafka_io import kafka_source
 from transformation.transformation import transform
 from stdio1 import stdoutput
+from pubsub.pubsub_destination import WriteToPubsub
 
 logging.basicConfig(level=logging.INFO)
+
 
 class Source(enum):
     """
     Source of the data
     """
+
     KAFKA = 1
     PUBSUB = 2
+
 
 class Destination(enum):
     """
     Destination of the message
     """
+
     STDIO = 1
     PUBSUB = 2
 
+
 # if set to true the transformation will be performed
-TRANSFORMATIONS = True
+TRANSFORMATIONS = False
+# The source of the data queue
+REQSOURCE = 1
+# the destination of the data queue
+DESTINATIONQ = 0
+
+PROJECT_ID = "robotic-augury-333723"
+TOPIC_NAME = "test"
+SERVICE_ACCOUNT_INFO = "service-account-info"
+
 
 def run_application() -> None:
     """
@@ -38,14 +54,28 @@ def run_application() -> None:
     Returns:
         None
     """
+
     logging.info("Starting the application")
-    value = kafka_source.kafka_read_data({"bootstrap_servers": "localhost:9092"}, "kafka-topic")
-    for msg in value:
-        
-        if TRANSFORMATIONS:
-            results = transform(msg.value,"decodebase64","./transformation/custom/decodebase64.py")
-        else:
-            results = msg.value
-        stdoutput.stdio(results)
-if __name__ == '__main__':
+    if Source.KAFKA.value == REQSOURCE:
+        value = kafka_source.kafka_read_data(
+            {"bootstrap_servers": "localhost:9092"}, "kafka-topic"
+        )
+        for msg in value:
+            if TRANSFORMATIONS:
+                results = transform(
+                    msg.value, "decodebase64", "./transformation/custom/decodebase64.py"
+                )
+            else:
+                results = msg.value
+            logging.info("Message: %s transformed to %s", msg.value, results)
+            if Destination.STDIO.value == DESTINATIONQ:
+                stdoutput.stdio(results)
+            else:
+                logging.info("Sending message to pubsub")
+                WriteToPubsub(
+                    project_id=PROJECT_ID, topic_name=TOPIC_NAME, message=results
+                ).publish()
+
+
+if __name__ == "__main__":
     run_application()
--- pubsub/pubsub_destination.py
+++ pubsub/pubsub_destination.py
@@ -1,3 +1,32 @@
 # This python package when used will publish messages to pubsub topic
 # Requirments are that you have a pubsub service and GCP project set up 
 # And also the google pubsub client
+
+import logging
+from google.cloud import pubsub_v1
+from google.auth import jwt
+
+class WriteToPubsub:
+    """_summary_
+    """
+    def __init__(self, project_id, topic_name, message) -> None:
+        self.project_id = project_id
+        self.topic_name = topic_name
+        self.message     = message
+    
+    def _topic_path(self)->str:
+        """_summary_
+
+        Returns:
+            _type_: str
+        """
+        return f"projects/{self.project_id}/topics/{self.topic_name}"
+    
+    def publish(self):
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = self._topic_path()
+        try:
+            publisher.publish(topic_path, self.message)
+            logging.info(f"Message published to {self.topic_name}")
+        except Exception as exe:
+            raise Exception(exe) from exe
